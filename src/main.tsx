61c40ca3cec680db3ed7bb2598232c0ff7b45be3
Amsyar Rasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2025-08-21 12:56:02 +0800

    chore: Setup `react-query`

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { enableMapSet } from "immer";
 import App from "./App.tsx";
 
 // Enable Map & Set support for Immer
 enableMapSet();
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</StrictMode>,
 );
