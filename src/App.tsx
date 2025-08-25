1d01d53107eb83b1e0c15cd50d8a9772610fb3b7
Amsyar Rasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2025-08-25 20:00:41 +0800

    chore: Move account manager as its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,134 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
 import {
 	DownloadIcon,
 	ImportIcon,
 	Settings,
 	SlidersHorizontal,
+	UserIcon,
 } from "lucide-react";
 import { domToPng } from "modern-screenshot";
+import { AccountManagerDialog } from "./components/account-manager-dialog";
 import CourseManagementSheet from "./components/course-management-sheet";
-import SignIn from "./components/sign-in";
 import { ThemeToggle } from "./components/theme-toggle";
 import TimetableCustomizer from "./components/timetable-customizer";
 import { Button } from "./components/ui/button";
 import WeeklyTimetable from "./components/weekly-timetable";
-import { authClient, signOut, useSession } from "./lib/auth/auth-client";
-import { getUserSubscriptionInfo } from "./lib/auth/helpers";
 import TechnoUniversityImporterDialog from "./lib/providers/techno-university-provider/importer-dialog";
-
-function AccountPanel() {
-	const session = useSession();
-
-	const {
-		data: userSubscriptionInfo,
-		isLoading,
-		error,
-	} = useQuery({
-		queryKey: ["userSubscriptionInfo", session.data?.user.id],
-		enabled: !!session.data,
-		queryFn: async () => {
-			if (session.data) {
-				const subInfo = await getUserSubscriptionInfo(session.data.user.id);
-				if (subInfo) return subInfo;
-			}
-
-			return null;
-		},
-	});
-
-	if (!session.data) {
-		return (
-			<SignIn>
-				<Button>Sign In</Button>
-			</SignIn>
-		);
-	}
-
-	if (error) {
-		return <div>Error loading supporter status: {error.message}</div>;
-	}
-
-	if (isLoading) {
-		return <div>Loading supporter status...</div>;
-	}
-
-	if (!session.data) {
-		return (
-			<SignIn>
-				<Button>Sign In</Button>
-			</SignIn>
-		);
-	}
-
-	const isSupporter =
-		userSubscriptionInfo?.status === "active" ||
-		userSubscriptionInfo?.status === "trialing";
-
-	return (
-		<div className="flex flex-col justify-center items-center text-center gap-4 py-4">
-			<span className="text-lg font-semibold text-center">
-				Welcome {session.data?.user.name}! You are currently{" "}
-				{isSupporter ? "a supporter!" : "not a supporter."}
-				<br />
-				{userSubscriptionInfo?.cancelAtPeriodEnd === true && (
-					<span className="text-sm font-normal">
-						Your subscription will cancel at{" "}
-						{userSubscriptionInfo?.periodEnd?.toDateString()}.
-					</span>
-				)}
-			</span>
-			<div className="flex gap-2 justify-center">
-				<Button
-					disabled={isSupporter}
-					onClick={async () => {
-						await authClient.subscription.upgrade({
-							plan: "supporter",
-							// TODO: Proper URLs for these
-							successUrl: "/",
-							cancelUrl: "/",
-						});
-					}}
-				>
-					Become a Supporter
-				</Button>
-				<Button
-					disabled={!isSupporter}
-					onClick={async () => {
-						await authClient.subscription.cancel({
-							returnUrl: "/",
-							subscriptionId: userSubscriptionInfo!.id,
-						});
-					}}
-				>
-					Unsubscribe
-				</Button>
-				<Button
-					disabled={
-						userSubscriptionInfo?.status !== "canceled" &&
-						!userSubscriptionInfo?.cancelAtPeriodEnd
-					}
-					onClick={async () => {
-						await authClient.subscription.restore({
-							subscriptionId: userSubscriptionInfo!.id,
-						});
-					}}
-				>
-					Restore
-				</Button>
-				<Button
-					onClick={async () => {
-						await authClient.subscription.billingPortal({
-							returnUrl: "/",
-						});
-					}}
-				>
-					Manage Subscription
-				</Button>
-				<Button onClick={() => void signOut()}>Sign out</Button>
-			</div>
-		</div>
-	);
-}
 
 function App() {
 	// TODO: Export button instead or both and show save file picker?
@@ -168,8 +52,12 @@ function App() {
 				<ThemeToggle />
 			</div>
 
-			<AccountPanel />
-
+			<AccountManagerDialog>
+				<Button variant="outline">
+					<UserIcon className="w-4 h-4" />
+					Manage Account
+				</Button>
+			</AccountManagerDialog>
 			<div className="m-4 flex flex-wrap justify-center gap-2">
 				<CourseManagementSheet>
 					<Button variant="outline">
---
+++ src/components/account-manager-dialog.tsx
@@ -0,0 +1,139 @@
+import { useQuery } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { authClient, signOut, useSession } from "~/lib/auth/auth-client";
+import { getUserSubscriptionInfo } from "~/lib/auth/helpers";
+import SignIn from "./sign-in";
+import { Button } from "./ui/button";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./ui/dialog";
+
+export function AccountManagerDialog({ children }: { children: ReactNode }) {
+	const session = useSession();
+
+	const {
+		data: userSubscriptionInfo,
+		isLoading,
+		error,
+	} = useQuery({
+		queryKey: ["userSubscriptionInfo", session.data?.user.id],
+		enabled: !!session.data,
+		queryFn: async () => {
+			if (session.data) {
+				const subInfo = await getUserSubscriptionInfo(session.data.user.id);
+				if (subInfo) return subInfo;
+			}
+
+			return null;
+		},
+	});
+
+	if (!session.data) {
+		return (
+			<SignIn>
+				<Button>Sign In</Button>
+			</SignIn>
+		);
+	}
+
+	if (error) {
+		return <div>Error loading supporter status: {error.message}</div>;
+	}
+
+	if (isLoading) {
+		return <div>Loading supporter status...</div>;
+	}
+
+	if (!session.data) {
+		return (
+			<SignIn>
+				<Button>Sign In</Button>
+			</SignIn>
+		);
+	}
+
+	const isSupporter =
+		userSubscriptionInfo?.status === "active" ||
+		userSubscriptionInfo?.status === "trialing";
+
+	return (
+		<Dialog>
+			<DialogTrigger asChild>{children}</DialogTrigger>
+			<DialogContent className="sm:max-w-[95vh]">
+				<DialogHeader>
+					<DialogTitle>Manage Account</DialogTitle>
+					<DialogDescription>
+						Manage your subscription and account settings here.
+					</DialogDescription>
+				</DialogHeader>
+				<div className="flex flex-col justify-center items-center text-center gap-4 py-4">
+					<span className="text-lg font-semibold text-center">
+						Welcome {session.data?.user.name}! You are currently{" "}
+						{isSupporter ? "a supporter!" : "not a supporter."}
+						<br />
+						{userSubscriptionInfo?.cancelAtPeriodEnd === true && (
+							<span className="text-sm font-normal">
+								Your subscription will cancel at{" "}
+								{userSubscriptionInfo?.periodEnd?.toDateString()}.
+							</span>
+						)}
+					</span>
+					<div className="flex gap-2 justify-center">
+						<Button
+							disabled={isSupporter}
+							onClick={async () => {
+								await authClient.subscription.upgrade({
+									plan: "supporter",
+									// TODO: Proper URLs for these
+									successUrl: "/",
+									cancelUrl: "/",
+								});
+							}}
+						>
+							Become a Supporter
+						</Button>
+						<Button
+							disabled={!isSupporter}
+							onClick={async () => {
+								await authClient.subscription.cancel({
+									returnUrl: "/",
+									subscriptionId: userSubscriptionInfo!.id,
+								});
+							}}
+						>
+							Unsubscribe
+						</Button>
+						<Button
+							disabled={
+								userSubscriptionInfo?.status !== "canceled" &&
+								!userSubscriptionInfo?.cancelAtPeriodEnd
+							}
+							onClick={async () => {
+								await authClient.subscription.restore({
+									subscriptionId: userSubscriptionInfo!.id,
+								});
+							}}
+						>
+							Restore
+						</Button>
+						<Button
+							onClick={async () => {
+								await authClient.subscription.billingPortal({
+									returnUrl: "/",
+								});
+							}}
+						>
+							Manage Subscription
+						</Button>
+						<Button onClick={() => void signOut()}>Sign out</Button>
+					</div>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+}
